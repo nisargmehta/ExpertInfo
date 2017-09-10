beb9ec0af297a4d5c5176ce520d8723fef48b74d
nisargmehta <NisargMehta@Nisargs-Mac.local>
2017-09-09 19:23:18 -0700

    parse the information coming from the url request

---
+++ AppStoreInfo/AppDetails.swift
@@ -0,0 +1,36 @@
+//
+//  AppDetails.swift
+//  AppStoreInfo
+//
+//  Created by Nisarg Mehta on 9/9/17.
+//  Copyright © 2017 Open Source. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AppDetails {
+    var appName: String?
+    var imageUrl: String?
+    var appVersion: String?
+    var description: String?
+    var rating: Float?
+    var iconImage: UIImage?
+    
+//    init() {
+//        self.appName = "name"
+//        self.imageUrl = "blank"
+//        self.appVersion = "1"
+//        self.description = "desc"
+//        self.rating = 4
+//    }
+//    
+//    convenience init(name: String, image: String, version: String, desc: String, rating: Float) {
+//        self.init()
+//        self.appName = name
+//        self.imageUrl = image
+//        self.appVersion = version
+//        self.description = desc
+//        self.rating = rating
+//    }
+}
--- AppStoreInfo/ParseInfo.swift
+++ AppStoreInfo/ParseInfo.swift
@@ -17,24 +17,51 @@ class ParseInfo {
     
     func getDataForKeyword(keyWord : NSString) {
         let session = URLSession.shared
-        let request = NSMutableURLRequest(url: NSURL(string: String(format: "https://itunes.apple.com/search?entity=software&term=%@&limit=1",keyWord)) as! URL)
+        let request = NSMutableURLRequest(url: NSURL(string: String(format: "https://itunes.apple.com/search?entity=software&term=%@&limit=50",keyWord)) as! URL)
         let task = session.dataTask(with: request as URLRequest){
             (data, response, error) -> Void in
             if error != nil {
                 print("error in url")
             } else {
-                print(data!)
+//                print(data!)
                 do {
-                    
-                    let parsedData = try JSONSerialization.jsonObject(with: data!) as! [String:Any]
-                    print(parsedData)
-//                    let currentTemperatureF = currentConditions["temperature"] as! Double
-//                    print(currentTemperatureF)
+//                    let parsedData = try JSONSerialization.jsonObject(with: data! ) as! [String:Any]
+                    let parsedData = try JSONSerialization.jsonObject(with: data!, options: [.mutableContainers])
+                    let resultDict = parsedData as! NSDictionary
+                    self.createAppDetailsArray(results: resultDict)
                 } catch let error as NSError {
                     print(error)
                 }
             }
         }
         task.resume()
+    }
+    
+    func createAppDetailsArray(results: NSDictionary) {
+        let allApps = NSMutableArray()
+        let allResults = results.object(forKey: "results") as! NSArray
+        for case let appDict as NSDictionary in allResults {
+            let details = AppDetails()
+            if let name = appDict.object(forKey: "trackName") {
+                details.appName = name as? String
+//                print(name)
+            }
+            if let desc = appDict.object(forKey: "description") {
+                details.description = desc as? String
+            }
+            if let rate = appDict.object(forKey: "rating") {
+                details.rating = rate as? Float
+            }
+            if let image = appDict.object(forKey: "artworkUrl512") {
+                details.imageUrl = image as? String
+            }
+            if let version = appDict.object(forKey: "version") {
+                details.appVersion = version as? String
+            }
+            allApps.add(details)
+        }
+        if (self.delegate != nil) {
+            self.delegate?.didGetData(data: allApps)
+        }
     }
 }
