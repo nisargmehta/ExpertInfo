ea4a7a4f9b241708c32b9b98abb29504b8e133dc
nisargmehta <NisargMehta@Nisargs-Mac.local>
2017-09-09 19:24:00 -0700

    manage lazy loading of the images

--- AppStoreInfo/ImageDownloader.swift
+++ AppStoreInfo/ImageDownloader.swift
@@ -7,10 +7,30 @@
 //
 
 import Foundation
+import UIKit
 
-class ImageDownloader: Operation {
+class ImageDownloader {
+    var details: AppDetails?
+    var sessionTask: URLSessionTask? = nil
+    var completionHandler: ((Void)->Void)?
     
-    override func main() {
-        
+    func startDownload() {
+        let session = URLSession.shared
+        let request = NSMutableURLRequest(url: NSURL(string: (self.details?.imageUrl)!) as! URL)
+        self.sessionTask = session.dataTask(with: request as URLRequest){
+            (data, response, error) -> Void in
+            if error != nil {
+                print("error in url")
+            } else {
+                let image = UIImage.init(data: data!)
+                self.details?.iconImage = image
+                self.completionHandler?()
+            }
+        }
+        self.sessionTask?.resume()
+    }
+    
+    func cancelDownload() {
+        self.sessionTask?.cancel()
     }
 }
--- AppStoreInfo/ViewController.swift
+++ AppStoreInfo/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var appInfoTableView: UITableView!
     
+    var isLoading: Bool = false
     var parse: ParseInfo?
     
     lazy var allEnteries: NSMutableArray = {
@@ -26,6 +27,12 @@ class ViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate
         self.parse?.delegate = self
         
         self.searchBar.delegate = self
+        
+        self.appInfoTableView.delegate = self;
+        self.appInfoTableView.dataSource = self;
+//        self.present(self, animated: false, completion: nil)
+        
+        self.appInfoTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,12 +42,20 @@ class ViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate
 
     // MARK: Protocol
     func didGetData(data: NSMutableArray) {
-        
+        self.allEnteries = data
+        DispatchQueue.main.async{
+            self.isLoading = false;
+            self.appInfoTableView.reloadData()
+        }
     }
     
     // MARK: IBActions
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        self.parse?.getDataForKeyword(keyWord: self.searchBar.text! as NSString)
+        if (self.searchBar.text?.characters.count)! > 0 {
+            self.isLoading = true
+            self.searchBar.resignFirstResponder()
+            self.parse?.getDataForKeyword(keyWord: self.searchBar.text! as NSString)
+        }
     }
     
     // MARK: Tableview
@@ -53,7 +68,17 @@ class ViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
+        let details: AppDetails = self.allEnteries.object(at: indexPath.row) as! AppDetails
+        cell.textLabel?.text = details.appName
+        if let icon = details.iconImage {
+            cell.imageView?.image = icon
+        } else {
+            self.startImageDownload(details: details, index: indexPath)
+            cell.imageView?.image = UIImage.init(named: "placeholder.png")
+        }
+        
+        return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -62,6 +87,18 @@ class ViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
+    }
+    
+    func startImageDownload(details: AppDetails, index: IndexPath) {
+        let downloader = ImageDownloader()
+        downloader.details = details
+        downloader.completionHandler = {
+            DispatchQueue.main.async {
+                let theCell: UITableViewCell = self.appInfoTableView.cellForRow(at: index)!
+                theCell.imageView?.image = details.iconImage
+            }
+        }
+        downloader.startDownload()
     }
 }
 
