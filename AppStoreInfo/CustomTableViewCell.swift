cc4880de321ff3993ceec1baa0ec388ec044dc86
nisargmehta <NisargMehta@Nisargs-Mac.local>
2017-09-10 23:37:09 -0700

    add custom table view cell that displays rating and other details

---
+++ AppStoreInfo/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  AppStoreInfo
+//
+//  Created by Nisarg Mehta on 9/10/17.
+//  Copyright © 2017 Open Source. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var theNameLabel: UILabel!
+    @IBOutlet weak var theImageView: UIImageView!
+    
+}
